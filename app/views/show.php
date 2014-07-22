17d1df627ded2647d17ebb85b11597541c7a2eb9
Karen Coombs <librarywebchic@gmail.com>
2014-07-22 11:42:24 -0500

    Added code to request a pulllist and parse it

--- app/views/show.php
+++ app/views/show.php
@@ -1,10 +1,17 @@
 <?php
+
+if (isset($_GET['branchId'])){
+    $branchID = $_GET['branchId'];
+}else{
+    $branchID = $config['institutions'][$_SESSION['institution']]['defaultBranch'];
+}
+
 $parameters = array();
 $parameters['principalID'] = $accessToken->getPrincipalID();
 $parameters['principalIDNS'] = $accessToken->getPrincipalIDNS();
-$parameters['inst'] = $accessToken->getContextInstitution();
+$parameters['inst'] = $_SESSION['institution'];
 
-$url = 'https://circ.sd' . $config['datacenter'] . '.worldcat.org/pulllist/' . $_GET['branchID'] . '?' . http_build_query($parameters, '', '&');
+$url = 'https://circ.sd' . $config['institutions'][$_SESSION['institution']]['datacenter'] . '.worldcat.org/pulllist/' . $branchID . '?' . http_build_query($parameters, '', '&');
 
 $client = new Client();
 $client->getClient()->setDefaultOption('config/curl/' . CURLOPT_SSLVERSION, 3);
@@ -14,11 +21,14 @@ $request = $client->createRequest('GET', $url, $headers);
 
 try {
     $response = $request->send();
+    $pulllistAtom = simplexml_load_string($response->getResponseBody());
+    $pulllistAtom->registerXPathNamespace("atom", "http://www.w3.org/2005/Atom");
+    $pulllistAtom->registerXPathNamespace("pulllist", "http://worldcat.org/xmlschemas/Circulation-1.0");
+    $pulllistAtom->registerXPathNamespace("bib", "http://worldcat.org/xmlschemas/Bib-1.0");
+    $pulllistItems = $results->xpath('/atom:feed/atom:entry/atom:content/pulllist:itemDescription');
     
 } catch (\Guzzle\Http\Exception\BadResponseException $error) {
     echo $error->getResponse()->getStatusCode();
-    echo $error->getResponse()->getWwwAuthenticate();
-    echo $error->getResponse()->getBody(true);
 }
 
 
@@ -37,8 +47,17 @@ body {
 <body>
 <?php 
 foreach ($pullistItems as $item){
-?>
+$bibliographicItem = $item->children('http://worldcat.org/xmlschemas/Bib-1.0');
+$title = $bibliographicItem->title;
+$callNumber = $item->callNumber->description;
+$barcode = $item->pieceDesignation;
 
+?>
+<div>
+<p>Title: <?php $title?></p>
+<p>Call Number<?php $callNumber?></p>
+<p>Barcode <?php $barcode?></p>
+</div>
 <?php 
 }?>
 </body>
