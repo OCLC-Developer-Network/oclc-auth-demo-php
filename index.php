d417e9aae4237cf0af1c999de4891b43996369f3
Karen Coombs <librarywebchic@gmail.com>
2014-07-23 08:58:31 -0500

    Added logic to handle when retrieving an Access Token creates an error

--- index.php
+++ index.php
@@ -2,41 +2,74 @@
 require 'vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
+
 use OCLC\Auth\WSKey;
 use OCLC\Auth\AuthCode;
 use OCLC\Auth\AccessToken;
+use OCLC\User;
 
 global $config;
 $config = Yaml::parse('app/config/config.yaml');
 
+$guzzleOptions = array(
+    'config' => array(
+        'curl' => array(
+            CURLOPT_SSLVERSION => 3
+        )
+    ),
+    'allow_redirects' => array(
+        'strict' => true
+    ),
+    'timeout' => 60
+);
+
+if (!class_exists('Guzzle')) {
+    \Guzzle\Http\StaticClient::mount();
+}
+
+if (isset($config['authorizationServer'])){
+    AuthCode::$authorizationServer = $config['authorizationServer'];
+    AccessToken::$authorizationServer = $config['authorizationServer'];
+    WSKey::$testServer = TRUE;
+    $guzzleOptions['verify'] = false;
+}
+
+
+
 /* Determine the redirect_uri of your application*/
 if (isset($_SERVER['HTTPS'])):
-$redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$redirect_uri = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
 else:
-$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
 endif;
 
 session_start();
  
 /* Construct a new WSkey object using the key, secret and an options array that contains the services you want to access and your redirect_uri */
-$options = array('services' => $services, 'redirectUri' => $redirect_uri);
-$wskey = new WSKey($key, $secret, $options);
+$options = array('services' => array('WMS_CIRCULATION', 'refresh_token'), 'redirectUri' => $redirect_uri);
+$wskey = new WSKey($config['wskey'], $config['secret'], $options);
  
 /* if you have an Access Token or Authorization Code already */
 if (isset($_SESSION['accessToken']) || isset($_GET['code'])) {
     /* if you have an Access Token or Authorization Code already */
     if (empty($_SESSION['accessToken'])) {
         /* if you do have an Authorization Code but not an Access Token, use the Authorization code to get an Access Token */
-        $accessToken = $wskey->getAccessTokenWithAuthCode($_GET['code'], 128807, 128807);
+        $accessToken = $wskey->getAccessTokenWithAuthCode($_GET['code'], $_SESSION['institution'], $_SESSION['institution']);
     
         $_SESSION['accessToken'] = $accessToken;
     } else {
         $accessToken = $_SESSION['accessToken'];
     }
-    include 'app/views/show.php';
+    if ($accessToken->getErrorCode()){
+        unset($_SESSION['accessToken']);
+        echo $accessToken->getResponse();
+        
+    }else {
+        include 'app/views/show.php';
+    }
 }elseif (isset($_GET['institution'])){ 
     /* if you don't have an Access token or Authorization Code, but know the institution redirect the user to the login URL */
-    $_SESSION['institution'] = $_GET['institution'];
+    $_SESSION['institution'] = (int)$_GET['institution'];
     header("Location: " . $wskey->getLoginURL($_SESSION['institution'], $_SESSION['institution']), 'true', '303');
 } else {
     /* if you don't have an Access token or Authorization Code and don't know what institution it is show the WAYF screen **/
